9bd9122dc57ef45ba56a39969f8c2a55829c57f4
abdullah-al-monir <abdullahalmonir364@gmail.com>
2025-02-28 17:35:49 +0600

    Created the first file with examples of declaring 3 types of variable

--- first.c
+++ first.c
@@ -16,17 +16,26 @@ int main()
   //   return 0;
   // }
 
-  float weight;
-  printf("Write your weight here: ");
-  scanf("%f", &weight);
-  if (weight < 70)
-  {
-    printf("You are underweight.");
-    return 0;
-  }
-  else if (weight >= 70 && weight < 80)
-  {
-    printf("You are normal weight.");
-    return 0;
-  }
+  // float weight;
+  // printf("Write your weight here: ");
+  // scanf("%f", &weight);
+  // if (weight < 70)
+  // {
+  //   printf("You are underweight.");
+  //   return 0;
+  // }
+  // else if (weight >= 70 && weight < 80)
+  // {
+  //   printf("You are normal weight.");
+  //   return 0;
+  // }
+
+  char name[30];
+
+  printf("Write your full name here: ");
+  fgets(name, sizeof(name), stdin);
+
+  printf("Hello, %s", name);
+
+  return 0;
 }
