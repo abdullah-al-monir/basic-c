2545fe3f7b23cfd97ad8227de4ec378e6c2c4c8a
abdullah-al-monir <abdullahalmonir364@gmail.com>
2025-02-28 17:27:14 +0600

    Basic C

---
+++ first.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+  // int age;
+  // printf("Write your age here: ");
+  // scanf("%d", &age);
+  // if (age < 18)
+  // {
+  //   printf("You are a kid.");
+  //   return 0;
+  // }
+  // else
+  // {
+  //   printf("You are an adult.");
+  //   return 0;
+  // }
+
+  float weight;
+  printf("Write your weight here: ");
+  scanf("%f", &weight);
+  if (weight < 70)
+  {
+    printf("You are underweight.");
+    return 0;
+  }
+  else if (weight >= 70 && weight < 80)
+  {
+    printf("You are normal weight.");
+    return 0;
+  }
+}
